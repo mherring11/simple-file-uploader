dc92dcbce5d9d33eb388611e236158c1c4c3d5e3
mherring11 <m.herring11@yahoo.com>
2022-11-27 13:09:17 -0600

    worled on js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,7 @@
-const form = document.getElementById('form'),
-    fileInput = document.getElementById('file-input'),
-    progressArea = document.getElementById('progress-area'),
-    uploadedArea = document.getElementById('uploaded-area');
-var ifRunning = false;
-
-// form click Event
-form.addEventListener("click", () => {
-    if (ifRunning) {
-        alert('Please wait another file is uploading..')
-        return
-    }
-
-    fileInput.click();
-})
-
+const form = document.querySelector("form"),
+fileInput = document.querySelector(".file-input"),
+progressArea = document.querySelector(".progress-area"),
+uploadedArea = document.querySelector(".uploaded-area");
+form.addEventListener("click", () =>{
+  fileInput.click();
+});
