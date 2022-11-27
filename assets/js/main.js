26956edb87487c0c2406827d6a52ef9da5089750
mherring11 <m.herring11@yahoo.com>
2022-11-27 14:12:16 -0600

    about to start php

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,20 @@
 const form = document.querySelector("form"),
-fileInput = document.querySelector(".file-input"),
+fileInput = form.querySelector(".file-input"),
 progressArea = document.querySelector(".progress-area"),
-uploadedArea = document.querySelector(".uploaded-area");
+uploadArea = document.querySelector(".upload-area");
+
 form.addEventListener("click", () =>{
   fileInput.click();
 });
+
+fileInput.onchange = ({target}) => {
+    let file = target.files[0];
+    if(file) {
+        let fileName = file.name;
+        uploadFile(fileName);
+    }
+}
+
+function uploadFile(name) {
+    
+}
